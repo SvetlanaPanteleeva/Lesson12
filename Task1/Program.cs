2dcae9c431733e7037d8a766303cfa8b66d35eb6
Светлана Пантелеева <panteleevas@gmail.com>
2023-02-05 22:01:39 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double r = Convert.ToDouble(Console.ReadLine());
+            double length = Сircle.GetLength(r);
+            double square = Сircle.GetSquare(r);
+            string point = Сircle.GetPoint(x, y, r);
+            Console.WriteLine($"{length}\n{square}");
+            Console.WriteLine($"{point}"); 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1/Сircle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+     static class Сircle
+     { 
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public string GetPoint(double x, double y, double r)
+
+        {
+           
+           double h = Math.Sqrt(x * x + y * y);
+
+            if (h > r)
+    
+                return "Точка не принадлежит окружности";
+            
+            else
+               return "Точка принадлежит окружности";
+            }
+        }
+
+    }
+     
+
